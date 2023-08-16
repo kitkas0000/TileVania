bfc529a4a4b36370a01302160bf95063d1312500
Nguyễn Đình Đức <ng.dinhduc.2311@gmail.com>
2023-08-16 17:20:43 +0700

    update restart game

--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -8,7 +8,10 @@ public class Level : MonoBehaviour
     [SerializeField] float levelLoadDelay = 1f;
     void OnTriggerEnter2D(Collider2D other)
     {
-        StartCoroutine(LoadNextLevel());
+        if(other.tag == "Player")
+        {
+            StartCoroutine(LoadNextLevel());
+        }
     }
 
     IEnumerator LoadNextLevel()
